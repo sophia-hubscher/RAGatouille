dead8b05d91d9807818fae2a6609cd9823e9724a
Dhevin Nandyala <dhevinnandyala@gmail.com>
2024-11-24 18:12:39 -0500

    added metrics.py

--- back-end/evaluation/metrics.py
+++ back-end/evaluation/metrics.py
@@ -1,157 +1,117 @@
 #Dataset Interface:
 import math
 import statistics
+import pandas as pd
+class Metrics:
 
+    #you only need to worry about init, eval_row, and macro_metrics. Ignore the rest.
 
-def getRow(index: int, table) -> tuple:
-    """
-    :param index: int (row number)
-    :returns: tuple:
-        question: str : question from the dataset
-        answer: str : correct answer from the dataset
-        ground_truth: list[str] : list of ground truth phrases from the dataset
-        relevant_docs: list[Any] : list of document identifiers from the dataset
-    """
+    def __init__(self):
+        self.results = []
 
-    question: str = ""
-    answer: str = ""
-    ground_truth: list[str] = []
-    relevant_docs: list = []
+    def precision(self, retrieved: list, relevant: list) -> float:
+        if len(retrieved) == 0: return 0
+        if len(relevant) == 0: return 0
 
-    #TODO: Add your interface code here:
+        true_positive: int = 0
+        for i in retrieved:
+            if i in relevant:
+                true_positive += 1
 
+        return true_positive / len(retrieved)
 
-    return question, answer, ground_truth, relevant_docs
+    def precision_at_k(self, retrieved: list, relevant: list, k: int) -> float:
+        assert k > 0, "k must be greater than 0"
+        return self.precision(retrieved[:k], relevant)
 
-#RAG interface:
-def generate(question: str) -> tuple:
-    """
+    def recall(self, retrieved: list, relevant: list) -> float:
+        if len(retrieved) == 0: return 0
+        if len(relevant) == 0: return 1
 
-    :param question:
-    :return: tuple
-        docs: document identifiers for comparison
-        chunks: actual retrieved chunks of text
-        answer: RAG generated answer
+        true_positive: int = 0
+        for i in retrieved:
+            if i in relevant:
+                true_positive += 1
 
-    """
-    docs: list = [] #doc IDs
-    chunks: list = [str] #actual chunks of text
-    answer: str = "" #generated answer
+        return true_positive/len(relevant)
 
-    #TODO: Add your interface code here:
+    def recall_at_k(self, retrieved: list, relevant: list, k: int) -> float:
+        assert k>0, "k must be greater than 0"
+        return self.recall(retrieved[:k], relevant)
 
-    return docs, chunks, answer
+    def f1(self, retrieved: list, relevant: list) -> float:
+        p = self.precision(retrieved, relevant)
+        r = self.recall(retrieved, relevant)
 
-#retrieval metrics:
+        denominator = p + r
+        if denominator == 0:
+            return 0
 
-def precision(retrieved: list, relevant: list) -> float:
-    if len(retrieved) == 0: return 0
-    if len(relevant) == 0: return 0
-
-    true_positive: int = 0
-    for i in retrieved:
-        if i in relevant:
-            true_positive += 1
-
-    return true_positive / len(retrieved)
-
-def precision_at_k(retrieved: list, relevant: list, k: int) -> float:
-    assert k > 0, "k must be greater than 0"
-    return precision(retrieved[:k], relevant)
-
-def recall(retrieved: list, relevant: list) -> float:
-    if len(retrieved) == 0: return 0
-    if len(relevant) == 0: return 1
-
-    true_positive: int = 0
-    for i in retrieved:
-        if i in relevant:
-            true_positive += 1
-
-    return true_positive/len(relevant)
-
-def recall_at_k(retrieved: list, relevant: list, k: int) -> float:
-    assert k>0, "k must be greater than 0"
-    return recall(retrieved[:k], relevant)
-
-def f1(retrieved: list, relevant: list) -> float:
-    p = precision(retrieved, relevant)
-    r = recall(retrieved, relevant)
-
-    denominator = p + r
-    if denominator == 0:
-        return 0
-
-    return (2 * p * r) / denominator
-
-def f1_at_k(retrieved: list, relevant: list, k: int) -> float:
-    assert k > 0, "k must be greater than 0"
-    return f1(retrieved[:k], relevant)
-
-#generation metrics:
-def accuracy(generated: str, correct: str):
-    return 0
-def relevance(generated: str, ground_truth: list):
-    return 0
-def groundedness(generated: str, retrieved: list):
-    return 0
-
-#running eval:
-def eval_row(index: int, table):
-
-    question, correct_answer, ground_truth, relevant_docs = getRow(index, table)
-
-    retrieved_docs, retrieved_chunks, generated_answer = generate(question)
-
-    #retrieval metrics:
-
-    p = precision(retrieved_docs, relevant_docs)
-    p_at_k = [precision_at_k(retrieved_docs, relevant_docs, i+1) for i in range(len(retrieved_docs))]
-
-    r = recall(retrieved_docs, relevant_docs)
-    r_at_k = [recall_at_k(retrieved_docs, relevant_docs, i+1) for i in range(len(retrieved_docs))]
-
-    f = f1(retrieved_docs, relevant_docs)
-    f_at_k = r_at_k = [f1_at_k(retrieved_docs, relevant_docs, i+1) for i in range(len(retrieved_docs))]
+        return (2 * p * r) / denominator
 
     #generation metrics:
+    def accuracy(self, generated: str, correct: str):
+        #TODO: Prompt template and LLM Call
+        return 0
+    def relevance(self, generated: str, ground_truth: list):
+        #TODO: Prompt template and LLM Call
+        return 0
+    def groundedness(generated: str, retrieved: list):
+        #TODO: Prompt template and LLM Call
+        return 0
 
-    a = accuracy(generated_answer, correct_answer)
-    rel = relevance(generated_answer, ground_truth)
-    g = groundedness(generated_answer, retrieved_chunks)
+    #running eval:
+    def eval_row(
+            self,
+            question: str, #question from the dataset
+            correct_answer: str, #the correct answer from the dataset
+            generated_answer: str, #answer generated by the RAG chain
+            relevant_docs: list, #relevant document IDs
+            retrieved_docs: list, #retrieved document IDs
+            retrieved_chunks: list, #retrieved chunks of text
+            ground_truth: list[str],  # list of ground truth phrases from the dataset
+    ):
 
-    #return:
-    return {
-        "precision": p,
-        "precision_at_k": p_at_k,
-        "recall": r,
-        "recall_at_k": r_at_k,
-        "f1_score": f,
-        "f1_score_at_k": f_at_k,
-        "accuracy": a,
-        "relevance": rel,
-        "groundedness": g
-    }
+        #retrieval metrics:
+        p = self.precision(retrieved_docs, relevant_docs)
+        p_at_k = [self.precision_at_k(retrieved_docs, relevant_docs, i+1) for i in range(len(retrieved_docs))]
 
-def eval(table):
-    metrics = [eval_row(i, table) for i in range(len(table))]
-    return metrics
+        r = self.recall(retrieved_docs, relevant_docs)
+        r_at_k = [self.recall_at_k(retrieved_docs, relevant_docs, i+1) for i in range(len(retrieved_docs))]
 
-if __name__ == "__main__":
-    #your code here:
+        f1 = self.f1(retrieved_docs, relevant_docs)
+        #generation metrics:
 
-    #get dataset table / df:
-    table = None
+        a = self.accuracy(generated_answer, correct_answer)
+        rel = self.relevance(generated_answer, ground_truth)
+        g = self.groundedness(generated_answer, retrieved_chunks)
 
-    results: list[dict] = eval(table)
+        #return:
+        result = {
+            "precision": p,
+            "precisions_at_k": p_at_k,
+            "recall": r,
+            "recalls_at_k": r_at_k,
+            "f1": f1,
+            "accuracy": a,
+            "relevance": rel,
+            "groundedness": g
+        }
+        self.results.append(result)
+        return result
 
-    #do whatever you need to with the results here
-    mean_recall = statistics.mean([result["recall"] for result in results])
-    mean_accuracy = statistics.mean([result["accuracy"] for result in results])
-    mean_relevance = statistics.mean([result["relevance"] for result in results])
-    mean_groundedness= statistics.mean([result["groundedness"] for result in results])
-    mean_recall_at_1 = statistics.mean([result["recall_at_k"][0] for result in results])
+    def macro_metrics(self):
+        #Run ONCE after running eval_row on all rows
+        mean_recall = statistics.mean([result["recall"] for result in self.results])
+        mean_accuracy = statistics.mean([result["accuracy"] for result in self.results])
+        mean_relevance = statistics.mean([result["relevance"] for result in self.results])
+        mean_groundedness = statistics.mean([result["groundedness"] for result in self.results])
+        mean_recall_at_1 = statistics.mean([result["recall_at_k"][0] for result in self.results])
 
-    #idk send them somewhere I guess?
-
-    exit(0)
+        return {
+            "mean_recall": mean_recall,
+            "mean_accuracy": mean_accuracy,
+            "mean_relevance": mean_relevance,
+            "mean_groundedness": mean_groundedness,
+            "mean_recall_at_1": mean_recall_at_1
+        }
